6565d2456059f7c57f9236c0f5ecac201f8e15fd
KitEA <kit.earf@gmail.com>
2024-04-20 21:16:07 +0400

    add types to functions

--- main.py
+++ main.py
@@ -10,18 +10,18 @@ def main():
     
     print(f"{num_of_words} words found in the document")
 
-def construct_path():
+def construct_path() -> str:
     script_dir = os.path.dirname(__file__)
     path_to_data = os.path.join(PATH_TO_BOOKS, PATH_TO_FILE)
     abs_file_path = os.path.join(script_dir, path_to_data)
 
     return abs_file_path
 
-def get_book_text(abs_file_path):
+def get_book_text(abs_file_path: str) -> str:
     with open(abs_file_path) as f:
         return f.read()
     
-def get_num_of_words(text):
+def get_num_of_words(text: str) -> int:
     words = text.split()
     return len(words)
 
